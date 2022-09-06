30f7330ce7c6ee2249998d82d94128b33c87c439
raiki-hirose <raiki.hirose@asia-quest.jp>
2022-09-06 14:31:43 +0900

    a

--- app/routes/web.php
+++ app/routes/web.php
@@ -16,7 +16,7 @@ use Illuminate\Support\Facades\Route;
 Route::group(['middleware' => 'auth'], function() {
     Route::get('/', 'HomeController@index')->name('home');
     Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
-    Route::get('/folders/{folder}/tasks', 'TaskController@index')->name('tasks.index');
+    Route::get('/folders/{id}/tasks', 'TaskController@index')->name('tasks.index');
     Route::get('/folders/create', 'FolderController@showCreateForm')->name('folders.create');
     Route::post('/folders/create', 'FolderController@create');
     Route::get('/folders/{id}/delete', 'FolderController@showDeleteForm')->name('folders.delete');
